b3ca7701a8f44a66d3bf2fdc28a8313fd45ea8fa
mariavd <maria.dimitrova@helsinki.fi>
2017-01-17 16:47:04 +0200

    Remove the test function

--- example.py
+++ example.py
@@ -7,12 +7,6 @@ def reverse_words(s):
     return ' '.join(words_reversed)
 
 
-def test_reverse_words():
-    assert reverse_words('dogs hate cats') == 'cats hate dogs'
-    assert reverse_words('dog eat dog') == 'dog eat dog'
-    assert reverse_words('one two three four') == 'four three two one'
-
-
 def get_word_lengths(s):
     """
     Returns a list of integers representing
